80e57176b60f48222dafc80fbbce0fc0c4b7f158
kaushal20383 <22631370+kaushal20383@users.noreply.github.com>
2021-10-19 17:57:04 +0530

    Update credential.py

--- credential.py
+++ credential.py
@@ -1,4 +1,4 @@
-USER_AGENT = 'ml5thsepApp'
-CLIENT_ID = '3jNGcr3iDe23h2m4ujsK8g'
-SECRET = 'MXBzv7nc0C4UoeGZsMItPkmEncIxZA'
-DEVELOPER = 'Background_Bass_1058'
+USER_AGENT = ''
+CLIENT_ID = ''
+SECRET = ''
+DEVELOPER = ''
